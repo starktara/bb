c36a03df6d3fd6a2fa23c71b63d30288f85f5f45
Ayush Singh <ayush.singh.96@gmail.com>
2019-08-12 12:21:10 +0530

    carousel and slider js initialized

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import emailIcon from '../src/assets/emailIcon.svg';
 import phoneIcon from '../src/assets/phone-icon.svg';
@@ -27,9 +27,45 @@ import faceBookIcon from '../src/assets/facebook-icon.svg';
 import twitterIcon from '../src/assets/twitter-icon.svg';
 import linkedinIcon from '../src/assets/linkedin-icon.svg';
 import instagramIcon from '../src/assets/instagram-icon.svg';
+import M from 'materialize-css';
 
 
 const App = () =>  {
+
+    useEffect(() => {
+        const testiminialMessages = [
+            {
+                name: 'Jai Kumar',
+                message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
+            },{
+                name: 'Jimi Hendrix',
+                message: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+            },{
+                name: 'Buckethead',
+                message: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+            },{
+                name: 'David Gilmour',
+                message: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English.'
+            },{
+                name: 'Slash',
+                message: 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful.'
+            }
+        ];
+        let topSlider = document.querySelectorAll('.slider');
+        let topSliderInstances = M.Slider.init(topSlider, {});
+        let carouselOptions = {
+            shift: -150,
+           padding: 800,
+           dist: -200,
+           onCycleTo: function(data){
+            let index = data.getAttribute('index');
+            let message = testiminialMessages[index].message;
+            let name = testiminialMessages[index].name;
+           }
+       }
+       let testimonialCarousel = document.querySelectorAll('.carousel');
+       let testimonialCarouselInstance = M.Carousel.init(testimonialCarousel, carouselOptions);
+    });
   return (
     <div className="App">
         <header className="header">
