a63adca22463910e09327da400ba4739155347b9
Krishna <krishnakant.singh.min12@gmail.com>
2019-08-19 18:10:29 +0530

    chages for server configuration

--- server/index.js
+++ server/index.js
@@ -4,8 +4,6 @@ const bodyParser = require("body-parser");
 const path = require("path");
 //Require route files
 const seedData = require("./apis/seedingData/seedingData");
-const { Client } = require('@elastic/elasticsearch');
-
 //middleware for body parser
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -13,51 +11,20 @@ app.use(bodyParser.json());
 app.use("/apis/seedData", seedData);
 
 
-const client = new Client({ node: 'http://localhost:9200' });
-async function run () {
-  // Let's start by indexing some data
-  await client.index({
-    index: 'game-of-thrones',
-    body: {
-      character: 'Ned Stark',
-      quote: 'Winter is coming.'
-    }
-  })
-  await client.index({
-    index: 'game-of-thrones',
-    body: {
-      character: 'Daenerys Targaryen',
-      quote: 'I am the mother of dragons.'
-    }
-  })
-  await client.index({
-    index: 'game-of-thrones',
-    // here we are forcing an index refresh,
-    // otherwise we will not get any result
-    // in the consequent search
-    refresh: true,
-    body: {
-      character: 'Tyrion Lannister',
-      quote: 'A mind needs books like a sword needs a whetstone.'
-    }
-  })
-  // Let's search!
-  const { body } = await client.search({
-    index: 'game-of-thrones',
-    body: {
-      query: {
-        match: {
-          quote: 'winter'
-        }
-      }
-    }
-  })
-  console.log(body.hits.hits)
+// Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
-run().catch(console.log)
 
 app.get('/', function (req, res) {
     res.send('Hello World')
   })
    
-app.listen(4200);
+const port = process.env.PORT || 4200;
+ 
+app.listen(port);
