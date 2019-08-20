8ed0b3d16b9915645618ba74abbb73d27d4b23f1
Krishna <krishnakant.singh.min12@gmail.com>
2019-08-20 12:50:56 +0530

    fix changes

--- client/src/index.js
+++ client/src/index.js
@@ -9,8 +9,9 @@ import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import authReducer from './store/reducers/auth';
 
+const composeEnhancers = compose;
 
-const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+// const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 
 const rootReducer = combineReducers({
   auth: authReducer
