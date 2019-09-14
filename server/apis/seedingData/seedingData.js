8d1639b401713b7b9a796b0c2d40a78a4091fdc5
Krishna <krishnakant.singh.min12@gmail.com>
2019-09-14 14:07:56 +0530

    add schema for sell data

--- server/apis/seedingData/seedingData.js
+++ server/apis/seedingData/seedingData.js
@@ -470,6 +470,51 @@ router.get("/createLeadDetail", (req, res) => {
 });
 
 
+/*create schema to store bike details*/
+router.get("/sellBikeDetails", (req, res) => {
+  async function run() {
+    await client.indices.create(
+      {
+        index: "leadDetail",
+        body: {
+          mappings: {
+            properties: {
+              id: { type: "integer" },
+              name: { type: "text" },
+              type: { type: "integer" },
+              brand: { type: "integer" },
+              model: { type: "integer" },
+              regnumber: { type: "text" },
+              descr: { type: "text" },
+              price: { type: "integer" },
+              state: { type: "text" },
+              city: { type: "text" },
+              loc: { type: "text" },
+              location: { type: "geo_point" },
+              myear: { type: "integer" },
+              mmonth: { type: "integer" },
+              kmdriven: { type: "integer" },
+              images: { type: "string" },
+              mimage: { type: "string" },
+              owner: { type: "string" },
+              cc: { type: "integer" },
+              bhp: { type: "integer" },
+              category: { type: "integer" },
+              mileage: { type: "integer" },
+              address:{type:"text"},
+              mobile:{type:"integer"}
+            }
+          }
+        }
+      },
+      { ignore: [400] }
+    );
+  }
+  run().catch(console.log);
+  res.json({ msg: "Index Created Sucessfully" });
+});
+
+
 router.get("/getAllBikes", (req, res) => {
   async function getData() {
     const { body } = await client.search({
