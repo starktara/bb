c53f5776e121a200194b29a3f00ce892eb5405fb
Krishna <krishnakant.singh.min12@gmail.com>
2019-10-24 11:43:42 +0530

    add login logic

--- server/apis/userDetails/userDetails.js
+++ server/apis/userDetails/userDetails.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
 const router = express.Router();
 const { Client } = require("@elastic/elasticsearch");
 const client = new Client({ node: "http://localhost:9200" });
 //details of seller and bike from sell page
+// Load input validation
+// Load User model
+
 router.get("/createUser", (req, res) => {
   async function run() {
     await client.indices.create(
@@ -77,6 +81,54 @@ router.post("/insertUserDetails", (req, res) => {
         res.send("successfully inserted");
       }
       upload().catch(console.log);
+    });
+  });
+});
+
+router.post("/login", (req, res) => {
+
+  // Form validation
+  const { errors, isValid } = validateLoginInput(req.body);
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+  const email = req.body.email;
+  const password = req.body.password;
+  // Find user by email
+  User.findOne({ email }).then(user => {
+    // Check if user exists
+    if (!user) {
+      return res.status(404).json({ emailnotfound: "Email not found" });
+    }
+    // Check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        // User matched
+        // Create JWT Payload
+        const payload = {
+          id: user.id,
+          name: user.name
+        };
+        // Sign token
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          {
+            expiresIn: 31556926 // 1 year in seconds
+          },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: "Bearer " + token
+            });
+          }
+        );
+      } else {
+        return res
+          .status(400)
+          .json({ passwordincorrect: "Password incorrect" });
+      }
     });
   });
 });
