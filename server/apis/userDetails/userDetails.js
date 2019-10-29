5d6945020374c2145f685554a72088566b7a0214
Krishna <krishnakant.singh.min12@gmail.com>
2019-10-29 13:55:32 +0530

    bcrypt msg

--- server/apis/userDetails/userDetails.js
+++ server/apis/userDetails/userDetails.js
@@ -143,35 +143,34 @@ router.post("/login", (req, res) => {
       return res.status(404).json({ emailnotfound: "Email not found" });
     }
 
-    //   // Check password
-    //   bcrypt.compare(password, user.password).then(isMatch => {
-    //     if (isMatch) {
-    //       // Create JWT Payload
-    //       const payload = {
-    //         id: user.id,
-    //         name: user.name
-    //       };
-    //       // Sign token
-    //       jwt.sign(
-    //         payload,
-    //         keys.secretOrKey,
-    //         {
-    //           expiresIn: 31556926 // 1 year in seconds
-    //         },
-    //         (err, token) => {
-    //           res.json({
-    //             success: true,
-    //             token: "Bearer " + token
-    //           });
-    //         }
-    //       );
-    //     } else {
-    //       return res
-    //         .status(400)
-    //         .json({ passwordincorrect: "Password incorrect" });
-    //     }
-    //   });
-    // });
+      // Check password
+      // bcrypt.compare(password.value, user.password).then(isMatch => {
+      //   if (isMatch) {
+      //     // Create JWT Payload
+      //     const payload = {
+      //       id: user.id,
+      //       name: user.name
+      //     };
+      //     // Sign token
+      //     jwt.sign(
+      //       payload,
+      //       keys.secretOrKey,
+      //       {
+      //         expiresIn: 31556926 // 1 year in seconds
+      //       },
+      //       (err, token) => {
+      //         res.json({
+      //           success: true,
+      //           token: "Bearer " + token
+      //         });
+      //       }
+      //     );
+      //   } else {
+      //     return res
+      //       .status(400)
+      //       .json({ passwordincorrect: "Password incorrect" });
+      //   }
+      // });
   }
   loginUser().catch(console.log);
 });
