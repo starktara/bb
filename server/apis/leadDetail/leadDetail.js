9a36600b95f51c0f11eba9c86a7f50701af88677
Ankit Singh <2501.ankit@gmail.com>
2019-12-11 12:32:04 +0530

    some fixes

--- server/apis/leadDetail/leadDetail.js
+++ server/apis/leadDetail/leadDetail.js
@@ -1,34 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const multer = require('multer');
-const cors = require('cors');
 const { Client } = require("@elastic/elasticsearch");
 const client = new Client({ node: "http://localhost:9200" });
 const  mailer = require('../../helper/mailer');
-
-router.use(express.static('public'))
-
-var storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'public/images/uploads')
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + '-' + file.originalname)
-  }
-});
-
-const upload = multer({ storage })
-
-router.use(cors());
-
-router.post('/upload', upload.single('image'), (req, res) => {
-  if (req.file)
-    res.json({
-      imageUrl: `images/uploads/${req.file.filename}`
-    });
-  else 
-    res.status("409").json("No Files to Upload.")
-});
 
 
 //details of seller and bike from sell page
