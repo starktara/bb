8a3558c6f2af0c67ddb1d315c734e41e959f5010
Ayush Singh <ayush.singh.96@gmail.com>
2019-12-17 12:20:51 +0530

    seeding data changes

--- client/src/shared/mappings/bike_models.js
+++ client/src/shared/mappings/bike_models.js
@@ -5,7 +5,7 @@ export const MODELS = [
     'Classic 350',
     'Pulsar 150',
     'Dio',
-    'Pulsar ₹200',
+    'Pulsar 200',
     'Maestro',
     'Discover 125',
     'Dominor',
@@ -18,5 +18,17 @@ export const MODELS = [
     'Activa',
     'Shine',
     'Classic 500',
-    'Unicorn 160'
+    'Unicorn 160',
+    'Fascino',
+    'Passion Plus',
+    'Pulsar 135',
+    'CB Hornet',
+    'Apache RTR 160',
+    'Avenger 220 Street',
+    'Centuro',
+    'SZ RR',
+    'Mastero Edge',
+    'Glamour i3s',
+    'Duet'
 ];
+/* Line number -2 is the model id*/
