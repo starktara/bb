ee2898d19c03d68555fc31f5dac6d727158f231b
krishna21august <krishnakant.singh.min12@gmail.com>
2019-12-29 10:54:05 +0530

    changes  for internal page

--- server/apis/categoryDetails/categoryDetails.js
+++ server/apis/categoryDetails/categoryDetails.js
@@ -5,85 +5,10 @@ const client = new Client({ node: "http://localhost:9200" });
 
 router.get("/getCategoryById", (req, res) => {
   let filterData = JSON.parse(req.query.filterData);
-  let sortKey = {};
-  let mustArray = [];
-  let shouldArray = [];
-  mustArray.push({
-    match: {
-      category: req.query.category
-    }
-  });
-  if (filterData != null) {
-    if (filterData.sort.column != null) {
-      let column = filterData.sort.column;
-      let order = filterData.sort.order;
-      sortKey[column] = {
-        order: order
-      };
-    } else {
-      sortKey["id"] = { order: "desc" };
-    }
-    if (filterData.myear.length > 0) {
-      let mYear = filterData.myear;
-      mustArray.push({
-        terms: {
-          myear: mYear
-        }
-      });
-    }
-    if (filterData.brand.length > 0) {
-      let brand = filterData.brand;
-      mustArray.push({
-        terms: {
-          brand: brand
-        }
-      });
-    }
-
-    if (filterData.budget.length > 0) {
-      let budget = filterData.budget;
-      var i = 1;
-      for (let value of budget) {
-        let rangeArr = value ? value.split("-") : ["1", "15000"];
-        let gte = rangeArr[0];
-        let lte = rangeArr[1];
-        if (i == 1) {
-          mustArray.push({
-            range: {
-              price: { gte: gte, lte: lte }
-            }
-          });
-        } else {
-          shouldArray.push({
-            range: {
-              price: { gte: gte, lte: lte }
-            }
-          });
-        }
-        i++;
-      }
-    }
-
-    if (filterData.kmdriven != null) {
-      mustArray.push({
-        range: {
-          kmdriven: { gte: 0, lte: filterData.kmdriven }
-        }
-      });
-    }
-    if (filterData.searchTerm != null) {
-      mustArray.push({
-        match_phrase: {
-          city: filterData.searchTerm
-        }
-      });
-    }
-  } else {
-    sortKey["id"] = { order: "desc" };
-  }
-
+  console.log(filterData);
+  console.log("dsdsd");
   async function getBikesForCategory() {
-    if (filterData == null) {
+    if (req.query.searchTerm) {
       const { body } = await client.search({
         index: "bike-details",
         body: {
@@ -119,6 +44,83 @@ router.get("/getCategoryById", (req, res) => {
       });
       res.send(body.hits.hits);
     } else {
+      let sortKey = {};
+      let mustArray = [];
+      let shouldArray = [];
+      mustArray.push({
+        match: {
+          category: req.query.category
+        }
+      });
+      if (filterData != null) {
+        if (filterData.sort.column != null) {
+          let column = filterData.sort.column;
+          let order = filterData.sort.order;
+          sortKey[column] = {
+            order: order
+          };
+        } else {
+          sortKey["id"] = { order: "desc" };
+        }
+        if (filterData.myear.length > 0) {
+          let mYear = filterData.myear;
+          mustArray.push({
+            terms: {
+              myear: mYear
+            }
+          });
+        }
+        if (filterData.brand.length > 0) {
+          let brand = filterData.brand;
+          mustArray.push({
+            terms: {
+              brand: brand
+            }
+          });
+        }
+
+        if (filterData.budget.length > 0) {
+          let budget = filterData.budget;
+          var i = 1;
+          for (let value of budget) {
+            let rangeArr = value ? value.split("-") : ["1", "15000"];
+            let gte = rangeArr[0];
+            let lte = rangeArr[1];
+            if (i == 1) {
+              mustArray.push({
+                range: {
+                  price: { gte: gte, lte: lte }
+                }
+              });
+            } else {
+              shouldArray.push({
+                range: {
+                  price: { gte: gte, lte: lte }
+                }
+              });
+            }
+            i++;
+          }
+        }
+
+        if (filterData.kmdriven != null) {
+          mustArray.push({
+            range: {
+              kmdriven: { gte: 0, lte: filterData.kmdriven }
+            }
+          });
+        }
+        if (filterData.searchTerm != null) {
+          mustArray.push({
+            match_phrase: {
+              city: filterData.searchTerm
+            }
+          });
+        }
+      } else {
+        sortKey["id"] = { order: "desc" };
+      }
+
       const { body } = await client.search({
         index: "bike-details",
         body: {
@@ -134,7 +136,6 @@ router.get("/getCategoryById", (req, res) => {
         }
       });
       res.send(body.hits.hits);
-
     }
   }
   getBikesForCategory().catch(console.log);
