b0879a0f30d1e35f53ec389266e8aeeea08cbad0
Ayush Singh <ayush.singh.96@gmail.com>
2020-01-18 12:59:57 +0530

    mobile nav search functionality

--- client/src/Components/MobileNav/MobNav.js
+++ client/src/Components/MobileNav/MobNav.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import './MobileNav.css';
 import { Link } from "react-router-dom";
 import logoPng from "../../assets/logo.png";
+import { makeStyles } from "@material-ui/core/styles";
 import userIcon from '../../assets/icons/user-icon.svg';
+import searchIcon from '../../assets/icons/search-icon.svg';
+
+const useStyles = makeStyles(theme => ({
+  searchIcon: {
+    position: 'absolute',
+    cursor: 'pointer',
+    top: '83px',
+    left: '25px',
+    width: '15px',
+    height: '15px',
+  }
+}));
 
 const MobNav = (() => {
+  const classes = useStyles();
+  const [searchTerm, setSearchTerm] = useState("");
+    const updateState = event => {
+      setSearchTerm(event.target.value);
+  };
   return (
     <nav className="BBMNav" role="navigation">
       <div id="BBMmenuToggle">
@@ -21,8 +39,16 @@ const MobNav = (() => {
       </div>
       <div className="bbMlogo"><Link to="/"><img src={logoPng} alt="" /></Link></div>
       <div className="BBMMyAcc"><img src={userIcon} alt="" /></div>
-           <div className="BBMsearchSec"><input className="BBMsearchField" type="text" placeholder="Search Your Two-wheeler" value="" onChange={() => { }} />
-      </div>
+            <div className="BBMsearchSec">
+              <Link to={`/category/bike?searchTerm=${searchTerm}&city=Aluva`}>
+                <img src={searchIcon} className={classes.searchIcon} alt=""/>
+              </Link>
+             <input className="BBMsearchField"
+              type="text"
+              placeholder="Search Your Two-wheeler"
+              value={searchTerm}
+              onChange={updateState} />
+            </div>
     </nav>
   )
 });
