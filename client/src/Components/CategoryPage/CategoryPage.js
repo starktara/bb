916e1189c74d19e6de4ad443c5f68d40896ec6a9
Ayush Singh <ayush.singh.96@gmail.com>
2020-02-24 11:59:36 +0530

    buy url click bug fix

--- client/src/Components/CategoryPage/CategoryPage.js
+++ client/src/Components/CategoryPage/CategoryPage.js
@@ -15,30 +15,30 @@ import Spinner from "../../Components/UI/Spinner/Spinner";
 import { Menu } from "../../shared/utility";
 import categoryData from "../../shared/mappings/category_data";
 
-const CategoryPage = (props) => {
+const CategoryPage = props => {
   const dispatch = useDispatch();
-  const { vehicles, filter, loading, currentData, selectedCity } = useSelector((state) => state.vehicleDetails);
+  const { vehicles, filter, loading, currentData, selectedCity } = useSelector(
+    state => state.vehicleDetails
+  );
   var category;
   var searchTerm;
-  const stateFilterData = { 
+  const stateFilterData = {
     ...filter,
-    city: selectedCity 
+    city: selectedCity
   };
 
   useEffect(() => {
     category = categoryData[props.match.params.category].id;
     if (props.history.location.search.trim() !== "") {
       const search = new URLSearchParams(props.history.location.search);
-      searchTerm = search.get("searchTerm") ? search.get("searchTerm") : '';
+      searchTerm = search.get("searchTerm") ? search.get("searchTerm") : "";
     }
     dispatch(actions.getVehicles(category, stateFilterData, searchTerm));
-  }, [props.history.location.search])
-
-
+  }, [props.history.location.search, props.match.params.category]);
 
   useEffect(() => {
     dispatch(actions.getPaginatedData(0, 9));
-  }, [vehicles])
+  }, [vehicles]);
 
   const onPageChanged = paginationData => {
     const { currentPage, totalPages, pageLimit } = paginationData;
@@ -79,33 +79,59 @@ const CategoryPage = (props) => {
     renderedVehicles = <h2>'No Vehicles Found!'</h2>;
   }
 
-  let navigation = categoryData[
-    props.match.params.category
-  ].name.replace("Bike", "Motorcycle").slice(0, -1).split(" ").join("-");
-  let heading = categoryData[props.match.params.category].name.replace(
-    "Bike",
-    "Motorcycle"
-  ).slice(0, -1).split(" ").join("-");
+  let navigation = categoryData[props.match.params.category].name
+    .replace("Bike", "Motorcycle")
+    .slice(0, -1)
+    .split(" ")
+    .join("-");
+  let heading = categoryData[props.match.params.category].name
+    .replace("Bike", "Motorcycle")
+    .slice(0, -1)
+    .split(" ")
+    .join("-");
   let text =
     "Motorcycles are available at easy EMI starting at ₹2,000*. Your  dream bike is not a distant dream now.";
   return (
     <div id="CategoryPage">
       <Header />
       <MainMenu />
-      <Grid container component="div" direction="row" justify="center" className="mtop40">
+      <Grid
+        container
+        component="div"
+        direction="row"
+        justify="center"
+        className="mtop40"
+      >
         <Grid item xs={11} md={11} sm={11} lg={11}>
-          <Banner navigation={navigation} heading={heading} text={text} path={props.location.pathname} />
+          <Banner
+            navigation={navigation}
+            heading={heading}
+            text={text}
+            path={props.location.pathname}
+          />
         </Grid>
         <Grid item xs={11} md={11} sm={11} lg={11}>
           <Grid container component="div" direction="row">
             <Navigation />
-            <Grid item xs={12} md={12} sm={12} lg={9} className="ProductListSec">
+            <Grid
+              item
+              xs={12}
+              md={12}
+              sm={12}
+              lg={9}
+              className="ProductListSec"
+            >
               <SortDropDown
                 title="Sort by"
                 list={Menu}
                 category={categoryData[props.match.params.category].id}
               />
-              <Grid container direction="row" component="div" className={containerClass}>
+              <Grid
+                container
+                direction="row"
+                component="div"
+                className={containerClass}
+              >
                 {renderedVehicles}
               </Grid>
               {paginations}
@@ -116,7 +142,6 @@ const CategoryPage = (props) => {
       <Footer />
     </div>
   );
-
-}
+};
 
 export default CategoryPage;
