f50cdb9d17c81888c86145a4b6466a1448c9d410
Ankit Singh <2501.ankit@gmail.com>
2020-06-10 15:33:24 +0530

    testimonial css fix

--- client/src/Components/Homepage/Homepage.js
+++ client/src/Components/Homepage/Homepage.js
@@ -65,19 +65,19 @@ const Homepage = (props) => {
             "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         },
         {
-          name: "Buckethead",
+          name: "Placeholder",
           message:
             "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
         },
         {
           name: "David Gilmour",
           message:
-            "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English."
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         },
         {
-          name: "Slash",
+          name: "Name",
           message:
-            "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful."
+            "Et tortor consequat id porta. Risus at ultrices mi tempus imperdiet. Natoque penatibus et magnis dis parturient montes nascetur. Elit at imperdiet dui accumsan sit. Euismod nisi porta lorem mollis aliquam ut porttitor. Adipiscing bibendum est ultricies integer quis auctor. Fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate sapien nec. Semper risus in hendrerit gravida rutrum quisque non tellus. Tellus elementum sagittis vitae et. Quis hendrerit dolor magna eget est lorem ipsum."
         }
       ];
       let carouselOptions = {
