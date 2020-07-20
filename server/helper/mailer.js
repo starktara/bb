5c821e65058a9871f5566f60c44ae7f1e8fc8d22
Ankit Singh <2501.ankit@gmail.com>
2020-07-20 15:58:26 +0530

    change mail server

--- server/helper/mailer.js
+++ server/helper/mailer.js
@@ -3,18 +3,21 @@ const fs = require('fs');
 
 async function mailer(output, subject, sendTo, attach = []) { 
     let transporter = nodemailer.createTransport({
-      host: "smtp.office365.com",
+      // host: "smtp.office365.com", // not working, hence replaced
+      host: "smtp.gmail.com",
       port: 587,
       secure: false, // true for 465, false for other ports
       auth: {
-        user: 'webadmin@bikebazaar.com',
-        pass: 'bikebaz@1981'
+        // user: 'webadmin@bikebazaar.com',
+        // pass: 'bikebaz@1981'
+        user: 'bikebazaar01@gmail.com',
+        pass: 'bikebazaar123'
       }
     });
 
     transporter.sendMail({
-      from: '"Trial" <webadmin@bikebazaar.com>', // sender address
-      to: sendTo, 
+      from: '"Trial" <bikebazaar01@gmail.com>', // sender address
+      to: 'sendTo', 
       subject: subject, // Subject line
       text: '', // plain text body
       html: output, // html body
